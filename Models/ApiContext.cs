1618946d9966db8ef75531ca97fd7de092914038
Bill Maintz <bill@allbusinesscomputerservices.com>
2020-06-26 14:21:21 -0500

    Add project files.

---
+++ Models/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvApi.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Placed { get; set; }
+        public DateTime? Completed { get; set; }
+
+    }
+}
